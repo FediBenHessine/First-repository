9113f886966806428aa0ed1f2d2476b074871924
FediBenHessine <160235539+FediBenHessine@users.noreply.github.com>
2024-02-27 02:21:22 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,43 @@
-'import' 
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculatrice calculatrice = new Calculatrice();
+        double a, b;
+        String operation;
+        double resultat;
+
+        System.out.println("Entrez le premier nombre :");
+        a = scanner.nextDouble();
+
+        System.out.println("Entrez le deuxième nombre :");
+        b = scanner.nextDouble();
+
+        System.out.println("Entrez l'opération à effectuer (+, -, *, /) :");
+        operation = scanner.next();
+
+        switch (operation) {
+            case "+":
+                resultat = calculatrice.addition(a, b);
+                break;
+            case "-":
+                resultat = calculatrice.soustraction(a, b);
+                break;
+            case "*":
+                resultat = calculatrice.multiplication(a, b);
+                break;
+            case "/":
+                try {
+                    resultat = calculatrice.division(a, b);
+                } catch (ArithmeticException e) {
+                    System.out.println(e.getMessage());
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Opération invalide.");
+                return;
+        }
+
+        System.out.println("Résultat : " + resultat);
+    }
+}

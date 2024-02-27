e1081b6c221adf094751b91d225954df8cb8aea5
FediBenHessine <160235539+FediBenHessine@users.noreply.github.com>
2024-02-27 02:20:27 +0100

    Update Calculatrice.java

--- Calculatrice.java
+++ Calculatrice.java
@@ -1,1 +1,23 @@
-'import'
+import java.util.Scanner;
+
+public class Calculatrice {
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    public double soustraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Division par zéro impossible.");
+        }
+        return a / b;
+    }
+}
+
